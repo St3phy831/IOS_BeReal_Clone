7dc0261b84eb7741557637eeec1576815dca983c
Stephanie Hernandez <stephernandez831@gmail.com>
2023-03-04 13:15:17 -0800

    Able to get authorization to photos

--- BeReal_Clone/Post/PostViewController.swift
+++ BeReal_Clone/Post/PostViewController.swift
@@ -6,24 +6,74 @@
 //
 
 import UIKit
+import PhotosUI
+import ParseSwift
 
 class PostViewController: UIViewController {
-
+    @IBOutlet weak var captionTextField: UITextField!
+    @IBOutlet weak var selectPhotoBtn: UIButton!
+    @IBOutlet weak var photoImage: UIImageView!
+    @IBOutlet weak var postBarItem: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func didTapAttachPhoto(_ sender: Any) {
+        // If authorized, show photo picker, otherwise request authorization.
+        // If authorization denied, show alert with option to go to settings to update authorization.
+        if PHPhotoLibrary.authorizationStatus(for: .readWrite) != .authorized {
+            // Request photo library access
+            PHPhotoLibrary.requestAuthorization(for: .readWrite) { [weak self] status in
+                switch status {
+                case .authorized:
+                    // The user authorized access to their photo library
+                    // show picker (on main thread)
+                    DispatchQueue.main.async {
+                        self?.presentImagePicker()
+                    }
+                default:
+                    // show settings alert (on main thread)
+                    DispatchQueue.main.async {
+                        // Helper method to show settings alert
+                        self?.presentGoToSettingsAlert()
+                    }
+                }
+            }
+        } else {
+            // Show photo picker
+            presentImagePicker()
+        }
     }
-    */
+    
+    private func presentImagePicker() {
+    }
+}
 
+// Helper methods to present various alerts
+extension PostViewController {
+    
+    /// Presents an alert notifying user of photo library access requirement with an option to go to Settings in order to update status.
+    func presentGoToSettingsAlert() {
+        let alertController = UIAlertController (
+            title: "Photo Access Required",
+            message: "In order to post a photo to complete a task, we need access to your photo library. You can allow access in Settings",
+            preferredStyle: .alert)
+        
+        let settingsAction = UIAlertAction(title: "Settings", style: .default) { _ in
+            guard let settingsUrl = URL(string: UIApplication.openSettingsURLString) else { return }
+            
+            if UIApplication.shared.canOpenURL(settingsUrl) {
+                UIApplication.shared.open(settingsUrl)
+            }
+        }
+        
+        alertController.addAction(settingsAction)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alertController.addAction(cancelAction)
+        
+        present(alertController, animated: true, completion: nil)
+    }
 }
