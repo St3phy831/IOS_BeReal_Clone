afe52cf24f77c193e649663cf23b4ffe65fa99f7
Stephanie Hernandez <stephernandez831@gmail.com>
2023-03-03 21:38:19 -0800

    Setup feed view

---
+++ BeReal_Clone/Feed/FeedViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FeedViewController.swift
+//  BeReal_Clone
+//
+//  Created by Stephanie Hernandez on 3/3/23.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
