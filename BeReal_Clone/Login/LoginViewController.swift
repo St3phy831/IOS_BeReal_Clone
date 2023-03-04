cee41063df82cb0dab533b94aadf458a1257def2
Stephanie Hernandez <stephernandez831@gmail.com>
2023-03-03 20:42:15 -0800

    Able to login

--- BeReal_Clone/Login/LoginViewController.swift
+++ BeReal_Clone/Login/LoginViewController.swift
@@ -16,16 +16,41 @@ class LoginViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
-    
 
-    /*
-    // MARK: - Navigation
+    @IBAction func onLoginTapped(_ sender: UIButton) {
+        // Make sure all fields are non-nil and non-empty.
+        guard let username = usernameTextField.text,
+            let password = passwordTextField.text,
+            !username.isEmpty,
+            !password.isEmpty else {
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+            showMissingFieldsAlert()
+            return
+        }
+
+        // Log in the parse user
+        User.login(username: username, password: password) { [weak self] result in
+            switch result {
+            case .success(let user):
+                print("✅ Successfully logged in as user: \(user)")
+
+            case .failure(let error):
+                self?.showAlert(description: error.localizedDescription)
+            }
+        }
     }
-    */
+    
+    private func showAlert(description: String?) {
+        let alertController = UIAlertController(title: "Unable to Log in", message: description ?? "Unknown error", preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default)
+        alertController.addAction(action)
+        present(alertController, animated: true)
+    }
 
+    private func showMissingFieldsAlert() {
+        let alertController = UIAlertController(title: "Opps...", message: "We need all fields filled out in order to log you in.", preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default)
+        alertController.addAction(action)
+        present(alertController, animated: true)
+    }
 }
