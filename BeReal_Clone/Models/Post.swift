bccdfa4c4b286e06884519f49f92ab3e6a83aa5e
Stephanie Hernandez <stephernandez831@gmail.com>
2023-03-04 12:57:04 -0800

    Created Post model

---
+++ BeReal_Clone/Models/Post.swift
@@ -0,0 +1,23 @@
+//
+//  Post.swift
+//  BeReal_Clone
+//
+//  Created by Stephanie Hernandez on 3/4/23.
+//
+
+import Foundation
+import ParseSwift
+
+struct Post: ParseObject {
+    // These are required by ParseObject
+    var objectId: String?
+    var createdAt: Date?
+    var updatedAt: Date?
+    var ACL: ParseACL?
+    var originalData: Data?
+
+    // Your own custom properties.
+    var caption: String?
+    var user: User?
+    var imageFile: ParseFile?
+}
