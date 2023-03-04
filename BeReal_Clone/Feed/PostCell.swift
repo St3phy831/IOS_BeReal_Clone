c8a161af3d0df39632d972481293b48fe8c03a0b
Stephanie Hernandez <stephernandez831@gmail.com>
2023-03-04 14:20:09 -0800

    Able to view posts on feed

--- BeReal_Clone/Feed/PostCell.swift
+++ BeReal_Clone/Feed/PostCell.swift
@@ -6,18 +6,58 @@
 //
 
 import UIKit
+import Alamofire
+import AlamofireImage
 
 class PostCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var postImageView: UIImageView!
+    @IBOutlet weak var captionLabel: UILabel!
+    
+    private var imageDataRequest: DataRequest?
+    
+    func configure(with post: Post) {
+        // Configure Post Cell
+        // Username
+        if let user = post.user {
+            usernameLabel.text = user.username
+        }
+        
+        // Image
+        if let imageFile = post.imageFile,
+           let imageUrl = imageFile.url {
+            
+            // Use AlamofireImage helper to fetch remote image from URL
+            imageDataRequest = AF.request(imageUrl).responseImage { [weak self] response in
+                switch response.result {
+                case .success(let image):
+                    // Set image view image with fetched image
+                    print("👍 Was able to get image")
+                    self?.postImageView.image = image
+                case .failure(let error):
+                    print("❌ Error fetching image: \(error.localizedDescription)")
+                    break
+                }
+            }
+        }
+        
+        // Caption
+        captionLabel.text = post.caption
+        
+        // Date
+        if let date = post.createdAt {
+            dateLabel.text = DateFormatter.postFormatter.string(from: date)
+        }
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
+    
+//    override func prepareForReuse() {
+//        super.prepareForReuse()
+//        // Cancel image download
+//        // Reset image view image.
+//        postImage.image = nil
+//
+//        // Cancel image request.
+//        imageDataRequest?.cancel()
+//    }
 }
