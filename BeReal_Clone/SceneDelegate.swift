22114a36e3fc67a9e9573c368367fc70c9e550a5
Stephanie Hernandez <stephernandez831@gmail.com>
2023-03-11 12:35:10 -0800

    Persisted login

--- BeReal_Clone/SceneDelegate.swift
+++ BeReal_Clone/SceneDelegate.swift
@@ -30,6 +30,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         NotificationCenter.default.addObserver(forName: Notification.Name("logout"), object: nil, queue: OperationQueue.main) { [weak self] _ in
             self?.logout()
         }
+        
+        if User.current != nil {
+            login()
+        }
     }
     
     private func login() {
